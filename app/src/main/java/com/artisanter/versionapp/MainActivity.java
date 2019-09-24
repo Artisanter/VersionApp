64218ae69ef12daaea4d68312bb00c3df51584c5
Artisanter <chyzhyk99@gmail.com>
2019-09-24 13:25:44 +0300

    Version displaying

--- app/src/main/java/com/artisanter/versionapp/MainActivity.java
+++ app/src/main/java/com/artisanter/versionapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.artisanter.versionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String version;
+        try{
+            version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch(PackageManager.NameNotFoundException e){
+            version = "unknown";
+        }
+        TextView tv = findViewById(R.id.version_tv);
+        tv.setText("Version: " + version);
     }
 }
